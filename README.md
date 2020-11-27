c4d6dddda4c2428f496b39490b466f689f99f17b
Brian McNamara <brianmay27@gmail.com>
2020-11-26 21:01:34 -0800

    Cleanup (#1)
    * Cleanup plugin
    
    Remove production arg, replacing with updateFile and writeToUpdateXml
    Update README
    More testing
    
    * Update readme
    
    * Add retryer
    
    * rename writeToUpdateXml
    
    * Add github actions
    
    * Fix exception logging
    
    * Add publish action

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # IntelliJ plugin uploader
 
-A gradle plugin to take care of uploading and updating updatePlugins.xml hosted on a private repository.
+A gradle plugin to take care of uploading an IntelliJ plugin and updating updatePlugins.xml hosted on a private repository.
 
 ## Usage
 
@@ -12,19 +12,41 @@ buildscript {
 }
 
 plugins {
-    id "dev.bmac.intellij.plugin-uploader" version "1.0.2"
+    id "dev.bmac.intellij.plugin-uploader" version "1.1.0"
 }
 
 uploadPlugin {
+    //Get the plugin distribution from the buildPlugin
     def archive = project.tasks.buildPlugin as Zip
     host 'https://repo.example.com/intellij/plugins/'
     pluginName 'PluginName'
+    file archive.archivePath
     pluginId project.group
     version project.version
-    production System.getenv("IS_PRODUCTION")
     description file('description.txt').text
     changeNotes file('change-notes.txt').text
     authentication 'Basic ' + new String(Base64.encoder.encode((username + ":" + password).bytes))
-    file archive.archivePath
 }
 ```
+
+## Configuration
+
+| Attributes | Values | 
+| :------------- | :--------- |
+| <kbd>host</kbd> - The host plus path of the repository to post the plugin to | **Required:** true <br/> **Acceptable Values:** <ul> <li>`https://repo.example.com/intellij/plugins` </li> <li>`http://repo.example.com:4443`</li></ul> |
+| <kbd>pluginName</kbd> - The plugin name to be used in the upload path such that host + pluginName is the folder the plugin will be uploaded to <br/><br/>**Note:** Name will be escaped when used as the upload path | **Required:** true <br/> **Acceptable Values:** Any String|
+| <kbd>file</kbd> - The file to be uploaded to the repo under host + pluginName + file.getName() |  **Required:** true <br/> **Acceptable Values:** A existing file path, ideally should be set via `project.tasks.buildPlugin as Zip` which grabs the file from the IntelliJ gradle plugin |
+| <kbd>pluginId</kbd> - Plugin Id used to match in the updatePlugins.xml | **Required:** true <br/> **Acceptable Values:** Any String|
+| <kbd>version</kbd> - Plugin version used to update updatePlugins.xml | **Required:** true <br/> **Acceptable Values:** Any String| 
+| <kbd>description</kbd> - Plugins description to be used in updatePlugins.xml | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** Any String| 
+| <kbd>changeNotes</kbd> - Plugins change notes to be used in updatePlugins.xml | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** Any String|
+| <kbd>authentication</kbd> - Authentication string used to publish files to the private repo. Will be used as the authorization header | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** <ul> <li> `Basic [authenticationString]` </li> <li> `Bearer [bearerToken] ` </li> </ul>
+| <kbd>updateFile</kbd> - Overrides the default updatePlugins.xml file name. | **Required:** false <br/> **Default:** <kbd>updatePlugins.xml</kbd> <br/> **Acceptable Values:** Any String |
+| <kbd>updatePluginXml</kbd> - Gates whether updatePlugins.xml is updated. | **Required:** false <br/> **Default:** <kbd>true</kbd> <br/> **Acceptable Values:** `true` / `false` |
+
+## Notes
+
+This plugin uses a lock file to prevent concurrent modifications to the updatePlugins.xml file.
+While the lock file will be cleaned up, it could be left behind if the process is forcefully interrupted
+requiring the lock to be deleted manually. The lock can be found in the <kbd>host</kbd> root and is named `updatePlugins.xml.lock`
+(lock file name depends on <kbd>updateFile</kbd>)
--- src/main/java/dev/bmac/gradle/intellij/IntellijPublishPlugin.java
+++ src/main/java/dev/bmac/gradle/intellij/IntellijPublishPlugin.java
@@ -1,5 +1,6 @@
 package dev.bmac.gradle.intellij;
 
+import com.github.rholder.retry.*;
 import com.sun.istack.Nullable;
 import okhttp3.*;
 import org.gradle.api.GradleException;
@@ -14,9 +15,12 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URLConnection;
-import java.nio.file.Files;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Simple gradle plugin to manage IntelliJ upload to a private repository as well as managing
@@ -24,15 +28,21 @@ import java.util.UUID;
  * Created by brian.mcnamara on Jan 29 2020
  **/
 public class IntellijPublishPlugin implements Plugin<Project> {
-    static final String UPDATE_PLUGIN = "updatePlugins.xml";
-    static final String UPDATE_PLUGIN_EAP = "updatePlugins-EAP.xml";
-    static final String LOCK_FILE = "publisher.lock";
+    static final String LOCK_FILE_EXTENSION = ".lock";
     private static final OkHttpClient CLIENT = new OkHttpClient.Builder().build();
 
     private final Marshaller marshaller;
     private final Unmarshaller unmarshaller;
+    private final int timeoutMs;
+    private final int retryTimes;
 
     public IntellijPublishPlugin() throws Exception {
+        this(1000, 5);
+    }
+
+    IntellijPublishPlugin(int timeoutMs, int retryTimes) throws Exception {
+        this.timeoutMs = timeoutMs;
+        this.retryTimes = retryTimes;
         JAXBContext contextObj = JAXBContext.newInstance(PluginUpdates.class);
 
         marshaller = contextObj.createMarshaller();
@@ -57,31 +67,76 @@ public class IntellijPublishPlugin implements Plugin<Project> {
                 extension.getFile() == null || extension.getPluginId() == null || extension.getVersion() == null) {
             throw new RuntimeException("Must specify host, pluginName, pluginId, version and file to uploadPlugin");
         }
+        if (extension.writeToUpdateXml() && extension.getUpdateFile() == null) {
+            throw new RuntimeException("updateFile can not be null");
+        }
+
+        postPlugin(extension, logger);
+        if (extension.writeToUpdateXml()) {
+            final AtomicReference<Throwable> firstException = new AtomicReference<>();
+            Retryer<Void> retryer = RetryerBuilder.<Void>newBuilder()
+                    .retryIfException()
+                    .withStopStrategy(StopStrategies.stopAfterAttempt(retryTimes))
+                    .withWaitStrategy(WaitStrategies.fixedWait(timeoutMs, TimeUnit.MILLISECONDS))
+                    .withRetryListener(new RetryListener() {
+                        @Override
+                        public <V> void onRetry(Attempt<V> attempt) {
+                            if (attempt.hasException()) {
+                                firstException.compareAndSet(null, attempt.getExceptionCause());
+                            }
+                        }
+                    })
+                    .build();
+            try {
+                retryer.call(() -> {
+                    postUpdateXml(extension, logger);
+                    return null;
+                });
+            } catch (ExecutionException | RetryException e) {
+                Throwable cause = firstException.get();
+                throw new GradleException("Failed to update " + extension.getUpdateFile(), cause != null ? cause : e);
+            }
+        }
+    }
+
+    void postUpdateXml(UploadPluginExtension extension, Logger logger) {
         String lock = null;
         try {
             lock = getLock(extension);
             if (lock != null) {
                 lock = null;
-                throw new GradleException("Lock exists on host. Can not proceed until lock is cleared");
+                throw new GradleException("Lock exists on host. Can not proceed until lock file is cleared." +
+                        " This could be another process currently running.");
             }
             lock = getLockId();
-            setLock(extension, lock);
+            setLock(extension, lock, logger);
             //TODO better lock safety
             if (!lock.equals(getLock(extension))) {
                 lock = null;
-                throw new GradleException("Another process claimed the lock. Please try again later.");
+                throw new GradleException("Another process claimed the lock while we were trying to claim it. Please try again later.");
             }
-            postPlugin(extension, logger);
             PluginUpdates.Plugin plugin = new PluginUpdates.Plugin(extension);
-            PluginUpdates updates = getUpdates(extension);
+            PluginUpdates updates = getUpdates(extension, logger);
             updates.updateOrAdd(plugin);
             postUpdates(extension, updates);
         } finally {
             if (lock != null) {
                 if (lock.equals(getLock(extension))) {
-                    clearLock(extension);
+                    Retryer<Object> retryer = RetryerBuilder.newBuilder()
+                            .retryIfExceptionOfType(IOException.class)
+                            .withStopStrategy(StopStrategies.stopAfterAttempt(retryTimes))
+                            .withWaitStrategy(WaitStrategies.fixedWait(timeoutMs, TimeUnit.MILLISECONDS))
+                            .build();
+                    try {
+                        retryer.call(() -> {
+                            clearLock(extension);
+                            return null;
+                        });
+                    } catch (ExecutionException | RetryException e) {
+                        logger.error("Failed to cleanup " + extension.getUpdateFile() + LOCK_FILE_EXTENSION + ". File must be cleaned up manually", e);
+                    }
                 } else {
-                    logger.error("The lock value changed during execution. This is bad! updatePlugins.xml may be invalid");
+                    logger.error("The lock value changed during execution. This is bad! " + extension.getUpdateFile() + " may be invalid");
                 }
             }
         }
@@ -92,18 +147,20 @@ public class IntellijPublishPlugin implements Plugin<Project> {
 
         RequestBody requestBody = RequestBody.create(extension.getFile(), MediaType.parse("application/zip"));
 
-        Request.Builder requestBuilder = new Request.Builder()
-                .url(extension.getHost() + pluginEndpoint)
-                .post(requestBody);
+        try {
+            Request.Builder requestBuilder = new Request.Builder()
+                    .url(URI.create(extension.getHost() + "/" + pluginEndpoint).normalize().toURL())
+                    .post(requestBody);
 
-        if (extension.getAuthentication() != null) {
-            requestBuilder.addHeader("Authorization", extension.getAuthentication());
-        }
-        Request request = requestBuilder.build();
+            if (extension.getAuthentication() != null) {
+                requestBuilder.addHeader("Authorization", extension.getAuthentication());
+            }
+            Request request = requestBuilder.build();
 
-        try (Response response = CLIENT.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new RuntimeException("Failed to upload plugin with status: " + response.code());
+            try (Response response = CLIENT.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new RuntimeException("Failed to upload plugin with status: " + response.code());
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -119,12 +176,12 @@ public class IntellijPublishPlugin implements Plugin<Project> {
             marshaller.marshal(updates, fw);
             fw.close();
 
-            String fileName = extension.isProduction() ? UPDATE_PLUGIN : UPDATE_PLUGIN_EAP;
+            String fileName = extension.getUpdateFile();
 
             RequestBody requestBody = RequestBody.create(file, MediaType.parse("application/xml"));
 
             Request.Builder requestBuilder = new Request.Builder()
-                    .url(extension.getHost() + fileName)
+                    .url(URI.create(extension.getHost() + "/" + fileName).normalize().toURL())
                     .post(requestBody);
 
             if (extension.getAuthentication() != null) {
@@ -134,7 +191,7 @@ public class IntellijPublishPlugin implements Plugin<Project> {
 
             try (Response response = CLIENT.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
-                    throw new RuntimeException("Failed to upload pluginUpdates.xml with status: " + response.code());
+                    throw new RuntimeException("Failed to upload " + fileName + " with status: " + response.code());
                 }
             }
         } catch (IOException | JAXBException e) {
@@ -142,26 +199,29 @@ public class IntellijPublishPlugin implements Plugin<Project> {
         }
     }
 
-    PluginUpdates getUpdates(UploadPluginExtension extension) {
-        Request request = new Request.Builder()
-                .url(extension.getHost() + (extension.isProduction() ? UPDATE_PLUGIN : UPDATE_PLUGIN_EAP))
-                .get()
-                .build();
+    PluginUpdates getUpdates(UploadPluginExtension extension, Logger logger) {
+        String updateFile = extension.getUpdateFile();
+        try {
+            Request request = new Request.Builder()
+                    .url(URI.create(extension.getHost() + "/" + updateFile).normalize().toURL())
+                    .get()
+                    .build();
 
-        try (Response response = CLIENT.newCall(request).execute()) {
-            if (response.code() == 404) {
-                System.out.println("No pluginUpdates.xml found");
-                return new PluginUpdates();
-            } else if (response.isSuccessful()) {
-                ResponseBody body = response.body();
-                if (body == null) {
-                    throw new RuntimeException("Body was null for updatePlugins");
+            try (Response response = CLIENT.newCall(request).execute()) {
+                if (response.code() == 404) {
+                    logger.info("No " + updateFile + " found. Creating new file.");
+                    return new PluginUpdates();
+                } else if (response.isSuccessful()) {
+                    ResponseBody body = response.body();
+                    if (body == null) {
+                        throw new RuntimeException("Body was null for " + updateFile);
+                    }
+                    return (PluginUpdates) unmarshaller.unmarshal(body.byteStream());
+                } else {
+                    throw new RuntimeException("Received an unknown status code while retrieving " + updateFile);
                 }
-                return (PluginUpdates) unmarshaller.unmarshal(body.byteStream());
-            } else {
-                throw new RuntimeException("Received an unknown status code while retrieving pluginUpdates.xml");
-            }
 
+            }
         } catch (IOException | JAXBException e) {
             throw new RuntimeException(e);
         }
@@ -169,35 +229,42 @@ public class IntellijPublishPlugin implements Plugin<Project> {
 
     @Nullable
     String getLock(UploadPluginExtension extension) {
-        Request request = new Request.Builder()
-                .url(extension.getHost() + LOCK_FILE)
-                .get()
-                .build();
+        try {
+            Request request = new Request.Builder()
+                    .url(URI.create(extension.getHost() + "/" + extension.getUpdateFile() + LOCK_FILE_EXTENSION).normalize().toURL())
+                    .get()
+                    .build();
 
-        try (Response response = CLIENT.newCall(request).execute()) {
-            if (response.code() == 404) {
-                return null;
-            } else if (response.isSuccessful()) {
-                ResponseBody body = response.body();
-                if (body == null) {
-                    return "";
+            try (Response response = CLIENT.newCall(request).execute()) {
+                if (response.code() == 404) {
+                    return null;
+                } else if (response.isSuccessful()) {
+                    ResponseBody body = response.body();
+                    if (body == null) {
+                        return "";
+                    }
+                    return body.string();
+                } else {
+                    throw new RuntimeException("Received an unknown status code while retrieving lock file, status: " + response.code());
                 }
-                return body.string();
-            } else {
-                throw new RuntimeException("Received an unknown status code while retrieving lock file, status: " + response.code());
-            }
 
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    void setLock(UploadPluginExtension extension, String lockValue) {
+    /**
+     * Upload the lock to the server.
+     * Note: This does not thow exceptions. It is expected to check the lock after this method is called to verify this
+     * process acquired the lock.
+     */
+    void setLock(UploadPluginExtension extension, String lockValue, Logger logger) {
         try {
             RequestBody requestBody = RequestBody.create(lockValue, MediaType.parse("text/plain"));
 
             Request.Builder requestBuilder = new Request.Builder()
-                    .url(extension.getHost() + LOCK_FILE)
+                    .url(URI.create(extension.getHost() + "/" + extension.getUpdateFile() + LOCK_FILE_EXTENSION).normalize().toURL())
                     .post(requestBody);
 
             if (extension.getAuthentication() != null) {
@@ -207,18 +274,18 @@ public class IntellijPublishPlugin implements Plugin<Project> {
 
             try (Response response = CLIENT.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
-                    throw new RuntimeException("Failed to upload lock with status: " + response.code());
+                    throw new IOException("Failed to upload lock with status: " + response.code());
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error("Failed to post lock file which will cause this process to fail when we read back the lock", e);
         }
     }
 
-    void clearLock(UploadPluginExtension extension) {
+    void clearLock(UploadPluginExtension extension) throws IOException {
         try {
             Request.Builder requestBuilder = new Request.Builder()
-                    .url(extension.getHost() + LOCK_FILE)
+                    .url(URI.create(extension.getHost() + "/" + extension.getUpdateFile() + LOCK_FILE_EXTENSION).normalize().toURL())
                     .delete();
 
             if (extension.getAuthentication() != null) {
@@ -228,11 +295,11 @@ public class IntellijPublishPlugin implements Plugin<Project> {
 
             try (Response response = CLIENT.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
-                    throw new RuntimeException("Failed to delete lock with status: " + response.code());
+                    throw new IOException("Failed to delete lock with status: " + response.code());
                 }
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Failed to parse the host", e);
         }
     }
 
