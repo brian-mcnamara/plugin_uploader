7d3c8bf2e47c30ba5df88a5032781b9c87487b19
Brian McNamara <brianmay27@gmail.com>
2021-07-02 13:00:51 -0700

    Version bump and README updates
    Logic cleanup

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ buildscript {
 }
 
 plugins {
-    id "dev.bmac.intellij.plugin-uploader" version "1.1.1"
+    id "dev.bmac.intellij.plugin-uploader" version "1.2.0"
 }
 
 uploadPlugin {
@@ -31,6 +31,7 @@ uploadPlugin {
     version project.version
     description file('description.txt').text
     changeNotes file('change-notes.txt').text
+    sinceBuild "211"
     // Example for Basic type authentication
     authentication 'Basic ' + new String(Base64.encoder.encode(("$username:$password").bytes))
 }
@@ -47,12 +48,31 @@ uploadPlugin {
 | <kbd>version</kbd> - Plugin version used to update updatePlugins.xml | **Required:** true <br/> **Acceptable Values:** Any String| 
 | <kbd>description</kbd> - Plugins description to be used in updatePlugins.xml | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** Any String| 
 | <kbd>changeNotes</kbd> - Plugins change notes to be used in updatePlugins.xml | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** Any String|
-| <kbd>sinceBuild</kbd> - Plugins minimum required IDE version. <br/><br/><b>Note:</b> This should match the version specided in plugin.xml. The IDE will still validate the version in plugin.xml if this is excluded, but will not be until its been downloaded. | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** A valid build number. See [Build Number Ranges](https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html) for more info.|
-| <kbd>untilBuild</kbd> - Plugins max allowed IDE version. <br/><br/><b>Note:</b> This should match the version specided in plugin.xml. The IDE will still validate the version in plugin.xml if this is excluded, but will not be until its been downloaded. | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** A valid build number. See [Build Number Ranges](https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html) for more info.|
+| <kbd>sinceBuild</kbd> - Plugins minimum required IDE version. <br/> See [Multi-versioning](#Multi-versioning) for more info. <br/><br/><b>Note:</b> This should match the version specified in plugin.xml. The IDE will still validate the version in plugin.xml if this is excluded, but will not be until its been downloaded. | **Required:** false (required if using multi-versioning) <br/> **Default:** *none* <br/> **Acceptable Values:** A valid build number. See [Build Number Ranges](https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html) for more info.|
+| <kbd>untilBuild</kbd> - Plugins max allowed IDE version. <br/><br/><b>Note:</b> This should match the version specified in plugin.xml. The IDE will still validate the version in plugin.xml if this is excluded, but will not be until its been downloaded. | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** A valid build number. See [Build Number Ranges](https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html) for more info.|
 | <kbd>authentication</kbd> - Authentication string used to publish files to the private repo. Will be used as the authorization header | **Required:** false <br/> **Default:** *none* <br/> **Acceptable Values:** <ul> <li> `Basic [authenticationString]` </li> <li> `Bearer [bearerToken] ` </li> </ul>
 | <kbd>updateFile</kbd> - Overrides the default updatePlugins.xml file name. <br/><br/><b>Note:</b> See [Publishing a Plugin to a Custom Plugin Repository](https://jetbrains.org/intellij/sdk/docs/basics/getting_started/update_plugins_format.html#describing-your-plugins-in-updatepluginsxml-file) for more information about updatePlugins.xml | **Required:** false <br/> **Default:** <kbd>updatePlugins.xml</kbd> <br/> **Acceptable Values:** Any String |
 | <kbd>updatePluginXml</kbd> - Gates whether updatePlugins.xml is updated. | **Required:** false <br/> **Default:** <kbd>true</kbd> <br/> **Acceptable Values:** `true` / `false` |
 | <kbd>uploadMethod</kbd> - Sets the HTTP method used for uploading files to the repo. | **Required:** false <br/> **Default:** <kbd>POST</kbd> <br/> **Acceptable Values:** <ul> <li>POST</li><li>PUT</li></ul> |
+
+
+## Multi-versioning
+
+As of version 1.2.0, multiple plugin entries with the same ID can be added to the <kbd>updateFile</kbd> file
+as long as the since-build and until-build don't overlap. This is available since IDEA version 193.2956.37 (2019.3)
+which added entry-filtering using since and until build on each entry.
+
+The gradle plugin will check if the current upload, or an existing entry on the repository, has a since-build set before 193.2956.37, 
+which prevents multiple entries from being added to ensure compatibility for the older IDEA versions.
+
+Otherwise, multi-versioning is enabled which will update an existing entry if the since-build matches otherwise
+a new entry is created and existing entries until-build are updated ensuring no overlap occurs. 
+
+For best experience, it is recommended to always provide a valid build version to the <kbd>sinceBuild</kbd> parameter
+and to exclude the <kbd>untilBuild</kbd> parameter as this plugin will take care of adding until-build when new entries are uploaded.
+It should be noted, specifying <kbd>untilBuild</kbd> is possible while using multi-version, however the until-build version may be changed
+at a later time to a lower build version (for example, if a new entry specifies a since-build which is before the current entries 
+until-build, the until-build will be updated to a build before the new entries since-build)
 
 ## Notes
 
@@ -63,5 +83,5 @@ requiring the lock to be deleted manually. The lock can be found in the <kbd>url
 
 ## License
 
-Most of this project is covered by MIT license - located in the LICENSE file, with certain portions covered by 
-Apache 2 license all of which clearly marked as such in a comment at the top of the file(s).
+Most of this project is covered under the MIT license - located in the LICENSE file, with certain portions covered by 
+Apache 2 license; all of which are clearly marked in a comment at the top of the file(s).
--- src/main/java/dev/bmac/gradle/intellij/PluginUpdatesUtil.java
+++ src/main/java/dev/bmac/gradle/intellij/PluginUpdatesUtil.java
@@ -1,13 +1,16 @@
 package dev.bmac.gradle.intellij;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.intellij.openapi.util.BuildNumber;
 import dev.bmac.gradle.intellij.xml.IdeaVersionElement;
 import dev.bmac.gradle.intellij.xml.PluginElement;
 import org.gradle.api.logging.Logger;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Utility for managing the repo plugin list.
@@ -19,61 +22,76 @@ public class PluginUpdatesUtil {
     /**
      * Updates or adds a plugin entry to the repo list. This has two different strategies based on the since-build of
      * the new entry and the existing ones in the repo.
-     * If any plugin entry for the current plugin ID has a since-build set before MIN_VERSION, this method will add or update
+     * If the new plugin entry has a since-build set before MIN_VERSION, this method will add or update
      * and existing entry ensuring there is only one entry per plugin ID.
      * However if that condition is not met, this method will use the "enhanced" logic to allow multiple plugin entries with
      * the same ID. If the since-build version is the same as an existing entry, that entry is updated, however if there is
      * no existing entries with that since-build, a new entry will be created and existing entries until-build will be updated
      * to ensure there is no version overlap between entries.
+     *
      * @param plugin the current plugin being uploaded
      * @param plugins the list of plugins from the server
      * @param logger logger from gradle
      */
     public static void updateOrAdd(PluginElement plugin, List<PluginElement> plugins, Logger logger) {
-        boolean allowsMultipleVersions = !hasLegacyPlugins(plugins);
-        if (allowsMultipleVersions && plugin.getVersionInfo() != null && plugin.getVersionInfo().getSinceBuild() != null) {
-            if (!versionAllowsMultipleEntries(plugin.getVersionInfo().getSinceBuild())) {
-                logger.warn("Notice: Multiple plugin versions are being used in updatePlugins.xml which requires IDEA version "
-                        + MIN_VERSION.asString() + " which is greater then this plugins since-build." +
-                        " Users on earlier versions may experience issues when using this repository. One fix is to update" +
-                        " the plugins since-build to be " + MIN_VERSION.asString() + " or greater.");
-                allowsMultipleVersions = false;
+        List<Integer> existingEntries = getExistingEntries(plugin, plugins);
+        boolean useMultiVersion = true;
+        if (existingEntries.size() > 1) {
+            if (plugin.getVersionInfo() != null && plugin.getVersionInfo().getSinceBuild() != null) {
+                if (versionDoesNotAllowMultiVersion(plugin.getVersionInfo().getSinceBuild())) {
+                    logger.warn("Notice: Multiple plugin versions are being used in updatePlugins.xml which requires IDEA version "
+                            + MIN_VERSION.asString() + " which is greater then this plugins since-build." +
+                            " Users on earlier versions may experience issues when using this repository. One fix is to update" +
+                            " the plugins since-build to be " + MIN_VERSION.asString() + " or greater.");
+                }
+            } else {
+                logger.error("The repository contains multi-version entries for this plugin which requires all uploads to set a since-build. " +
+                        "Please add a since-build to this upload and try again.");
+                return;
+            }
+        } else {
+            if (plugin.getVersionInfo() == null ||
+                    versionDoesNotAllowMultiVersion(plugin.getVersionInfo().getSinceBuild())) {
+                useMultiVersion = false;
+            } else if (existingEntries.size() == 1) {
+                PluginElement existingPlugin = plugins.get(existingEntries.get(0));
+                //If no since-build is set on existing entry, we will allow multi-version, however if the prior entry has
+                //a since-version before MIN_VERSION, we will override it and multi-version can kick in next time.
+                if (existingPlugin.getVersionInfo() != null && versionDoesNotAllowMultiVersion(existingPlugin.getVersionInfo().getSinceBuild())) {
+                    useMultiVersion = false;
+                }
             }
         }
 
-        if (!allowsMultipleVersions || plugin.getVersionInfo() == null || plugin.getVersionInfo().getSinceBuild() == null) {
+        if (!useMultiVersion || plugin.getVersionInfo() == null || plugin.getVersionInfo().getSinceBuild() == null) {
             logger.debug("Updating existing plugin entry or adding if none exists");
-            for (int i = 0; i < plugins.size(); i++) {
-                if (plugin.getId().equals(plugins.get(i).getId())) {
-                    plugins.set(i, plugin);
-                    return;
-                }
+            if (existingEntries.size() == 1) {
+                plugins.set(existingEntries.get(0), plugin);
+                return;
             }
         } else {
             Map<BuildNumber, PluginElement> buildNumberMap = Maps.newHashMap();
-            for (int i = 0; i < plugins.size(); i++) {
-                PluginElement existingPlugin = plugins.get(i);
-                if (plugin.getId().equals(existingPlugin.getId())) {
-                    //The same since since-build version or null version info (singly entry mode)
-                    if (existingPlugin.getVersionInfo() == null && plugin.getVersionInfo() == null ||
-                            (existingPlugin.getVersionInfo() != null &&
-                                    existingPlugin.getVersionInfo().getSinceBuild().compareTo(plugin.getVersionInfo().getSinceBuild()) == 0)) {
-                        logger.debug("Updating existing plugin entry as since-build version either does not exist or is identical.");
-                        plugins.set(i, plugin);
-                        return;
-                    } else if (existingPlugin.getVersionInfo() == null) {
-                        BuildNumber currentMinusOne = plugin.getVersionInfo().getSinceBuild().minusOne();
-                        logger.info("Adding idea-version with until-build set to " + currentMinusOne.asString() + " to existing plugin entry on the repository");
-                        existingPlugin.setVersionInfo(new IdeaVersionElement(null, currentMinusOne));
-                    }
+            for (Integer existingPosition : existingEntries) {
+                PluginElement existingPlugin = plugins.get(existingPosition);
+                //The same since since-build version or null version info (singly entry mode)
+                if (existingPlugin.getVersionInfo() == null && plugin.getVersionInfo() == null ||
+                        (existingPlugin.getVersionInfo() != null &&
+                                existingPlugin.getVersionInfo().getSinceBuild().compareTo(plugin.getVersionInfo().getSinceBuild()) == 0)) {
+                    logger.debug("Updating existing plugin entry as since-build version either does not exist or is identical.");
+                    plugins.set(existingPosition, plugin);
+                    return;
+                } else if (existingPlugin.getVersionInfo() == null) {
+                    BuildNumber currentMinusOne = plugin.getVersionInfo().getSinceBuild().minusOne();
+                    logger.info("Adding idea-version with until-build set to " + currentMinusOne.asString() + " to existing plugin entry on the repository");
+                    existingPlugin.setVersionInfo(new IdeaVersionElement(null, currentMinusOne));
+                }
 
-                    if (existingPlugin.getVersion().equals(plugin.getVersion())) {
-                        logger.error("Existing plugin entry with identical version detected in repository.");
-                    }
+                if (existingPlugin.getVersion().equals(plugin.getVersion())) {
+                    logger.error("Existing plugin entry with identical version detected in repository.");
+                }
 
-                    if (existingPlugin.getVersionInfo().getSinceBuild() != null) {
-                        buildNumberMap.put(existingPlugin.getVersionInfo().getSinceBuild(), existingPlugin);
-                    }
+                if (existingPlugin.getVersionInfo().getSinceBuild() != null) {
+                    buildNumberMap.put(existingPlugin.getVersionInfo().getSinceBuild(), existingPlugin);
                 }
             }
 
@@ -116,26 +134,20 @@ public class PluginUpdatesUtil {
         plugins.add(plugin);
     }
 
-    /**
-     * Returns true if an entry in pluginUpdates contains a version which is not compatible for multiple plugin entries
-     * using unique since-until versions. (See BuildNumberUtils.MIN_VERSION). Also will return false if there already
-     * exists multiple plugin entries.
-     */
-    private static boolean hasLegacyPlugins(List<PluginElement> plugins) {
-        boolean legacyPluginFound = false;
-        Set<String> ids = Sets.newHashSet();
-        for (PluginElement plugin : plugins) {
-            if (!ids.add(plugin.getId())) {
-                return false;
-            }
-            if (plugin.getVersionInfo() != null && plugin.getVersionInfo().getUntilBuild() != null) {
-                legacyPluginFound |= !versionAllowsMultipleEntries(plugin.getVersionInfo().getSinceBuild());
+    private static List<Integer> getExistingEntries(PluginElement pluginElement, List<PluginElement> plugins) {
+        List<Integer> entries = Lists.newArrayList();
+        for (int i = 0; i < plugins.size(); i++) {
+            if (plugins.get(i).getId().equals(pluginElement.getId())) {
+                entries.add(i);
             }
         }
-        return legacyPluginFound;
+        return entries;
     }
 
-    private static boolean versionAllowsMultipleEntries(BuildNumber version) {
-        return version.compareTo(MIN_VERSION) > 0;
+    private static boolean versionDoesNotAllowMultiVersion(BuildNumber version) {
+        if (version == null) {
+            return true;
+        }
+        return version.compareTo(MIN_VERSION) < 0;
     }
 }
